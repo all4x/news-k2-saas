f832c37866fbd846b0379d20d9da3cdf3104a012
allex <alex.devopse@gmail.com>
2024-07-08 00:38:40 -0300

    a

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ fastify.get('/news', async (request, reply) => {
 // Iniciar o servidor Fastify
 const start = async () => {
   try {
-    await fastify.listen({ port: 3032 });
+    await fastify.listen({ port: 80 });
     fastify.log.info(`Servidor Fastify rodando em http://localhost:80`);
   } catch (err) {
     fastify.log.error(`Erro ao iniciar o servidor: ${err}`);
