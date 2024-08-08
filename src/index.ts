b16d1da3be8cf73b6d4fb3e16d8cca8e15c6aeb4
allex <alex.devopse@gmail.com>
2024-08-08 18:03:24 -0300

    ajustes

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,14 @@ const fastify = Fastify({
   logger: true
 });
 
+// Função para limpar texto de links e tags HTML
+function limparTextoSemLinks(texto: string): string {
+  texto = texto.replace(/https?:\/\/\S+/g, '');
+  texto = texto.replace(/<[^>]*>/g, '');
+  texto = texto.replace(/\s+/g, ' ').trim();
+  return texto;
+}
+
 // Função para realizar o web scraping e obter as últimas 10 notícias
 async function scrapeData(): Promise<Article[] | undefined> {
   try {
@@ -28,14 +36,13 @@ async function scrapeData(): Promise<Article[] | undefined> {
     const articles: Article[] = [];
 
     $('.elementor.elementor-527201.e-loop-item').each((index, element) => {
-      if (index < 10) { // Limita a 10 artigos
-        const title = $(element).find('h3').text().trim(); // Extrai o título da notícia
-        const link = $(element).find('a').attr('href'); // Extrai o link da notícia
+      if (index < 10) {
+        const title = $(element).find('h3').text().trim();
+        const link = $(element).find('a').attr('href');
         let img = $(element).find('a > img').attr('data-lazy-src') || $(element).find('a > img').attr('src');
         const time = $(element).find('time').text();
 
         if (title && link) {
-          // Converte o URL da imagem para absoluto se for relativo
           const imageUrl = img ? (img.startsWith('http') ? img : `https://canalsolar.com.br${img}`) : undefined;
           articles.push({ title, link, img: imageUrl, time });
         }
@@ -49,30 +56,18 @@ async function scrapeData(): Promise<Article[] | undefined> {
   }
 }
 
+// Função para obter o conteúdo de uma notícia específica
 async function scrapeContent(url: string): Promise<ArticleContent | undefined> {
-
-  function limparTextoSemLinks(texto: string): string {
-    // Remove links
-    texto = texto.replace(/https?:\/\/\S+/g, '');
-    // Remove tags HTML
-    texto = texto.replace(/<[^>]*>/g, '');
-    // Remove espaços em branco extras
-    texto = texto.replace(/\s+/g, ' ').trim();
-    return texto;
-  }
-
   try {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
 
     const title = $('h1').text().trim();
     const img = $('img').first().attr('src');
-    let content = $('.elementor-widget-container').text().trim(); // Seleciona o conteúdo pela classe
+    let content = $('.elementor-widget-container').text().trim();
 
-    // Limpa o conteúdo de espaços extras e quebras de linha
     content = content.replace(/\n\s*\n/g, '\n').trim();
-
-    const cleanContent = limparTextoSemLinks(content)
+    const cleanContent = limparTextoSemLinks(content);
 
     if (title && content) {
       return { title, img, cleanContent };
@@ -104,7 +99,6 @@ fastify.get('/news-content', async (request, reply) => {
   }
 
   const content = await scrapeContent(url);
-  console.log(content)
   if (content) {
     return content;
   } else {
@@ -118,9 +112,8 @@ const start = async () => {
     await fastify.listen({
       host: '0.0.0.0',
       port: process.env.PORT ? Number(process.env.PORT) : 3333
-
     });
-    fastify.log.info(`Servidor Fastify rodando em http://localhost:80`);
+    fastify.log.info(`Servidor Fastify rodando em http://localhost:3333`);
   } catch (err) {
     fastify.log.error(`Erro ao iniciar o servidor: ${err}`);
     process.exit(1);
